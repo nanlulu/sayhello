ffe73caff90b84205e3ccb838fd827afafbfc148
Lu Chen <nanlulu@fb.com>
2020-04-04 18:37:35 -0700

    URI -> URL

--- sayhello/settings.py
+++ sayhello/settings.py
@@ -22,4 +22,4 @@ dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', dev_db)
